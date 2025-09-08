69083dd3fd302c5a081f36a39a8b205e32de12c1
Sanket Jadhav <jadhavsaket.2907@gmail.com>
2025-09-08 13:05:36 +0530

    implemanted search logic, applied css effects, added new fields to appuser model to add user bio, hobbies and details. Also added an option to change user details

--- social_app/user_profile/urls.py
+++ social_app/user_profile/urls.py
@@ -10,9 +10,15 @@ urlpatterns = [
     path('logout/', views.Logout.as_view(), name='logout'),
     path('profile/', views.Profile.as_view(), name='profile'),
     path('otheruserprofile/', views.OtherUserProfile.as_view(), name='otheruserprofile'),
+    path('friends/', views.FriendsList.as_view(), name='friends'),
+    path('search/', views.SearchResult.as_view(), name='search'),
+    path('editprofiledetails/', views.EditProfile.as_view(), name='editprofile'),
+    path('addotherdetails/', views.AddProfileDetails.as_view(), name='addotherdetails'),
+
+    # api endpoints
     path('api/like/<int:post_id>/', views.PostLikeCheck.as_view(), name='postlike'),
     path('api/friendreqto/<int:other_user_id>/', views.FriendRequestCheck.as_view(), name='friendrequestcheck'),
-    path('friends/', views.FriendsList.as_view(), name='profile'),
-    path('api/reqaccept/<int:received_req_id>/', views.FriendRequestAction.as_view(), name='friendrequestaction' )
-
+    path('api/reqaccept/<int:received_req_id>/', views.FriendRequestAction.as_view(), name='friendrequestaction'),
+    path('api/postcomment/<int:post_id>/', views.AddComment.as_view(), name='addcomment'),
+    path('api/deletepost/<int:post_id>/', views.DeletePost.as_view(), name='deletepost'),
 ]
